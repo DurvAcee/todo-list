a68aff90c42e4b7dd8e71a4dd7766bcd1313e0dd
Durvesh Danve <Durvesh.Danve@veritas.com>
2023-07-20 12:26:52 +0530

    Created todo model class

--- src/app/MyComponenets/todos/todos.component.ts
+++ src/app/MyComponenets/todos/todos.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo";
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+
+  todos: Todo[];
+  constructor(){
+    this.todos = [
+      {
+        sno: 1,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 3,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
